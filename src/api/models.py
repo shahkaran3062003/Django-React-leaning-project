01603e2a3db35459322f76779f03850a77e610d8
shahkaran3062003 <47536079+shahkaran3062003@users.noreply.github.com>
2024-04-11 17:52:46 +0530

    create new serializer to handle post request for creating new room.

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import random
 import string
+from datetime import datetime
 # Create your models here.
 
 
@@ -15,8 +16,8 @@ def gen_uniqe_code():
 
 
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(max_length=8, default=gen_uniqe_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
